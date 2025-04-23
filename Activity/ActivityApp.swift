eb0e91a1e92b0890e2c1bce4c7664deeeb8cf5a3
User <user@example.com>
2025-04-24 00:21:44 +0300

    Initial Commit

---
+++ Activity/ActivityApp.swift
@@ -0,0 +1,17 @@
+//
+//  ActivityApp.swift
+//  Activity
+//
+//  Created by Bahar Atbaş on 24.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct ActivityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ActivityTests/ActivityTests.swift
@@ -0,0 +1,17 @@
+//
+//  ActivityTests.swift
+//  ActivityTests
+//
+//  Created by Bahar Atbaş on 24.04.2025.
+//
+
+import Testing
+@testable import Activity
+
+struct ActivityTests {
+
+    @Test func testExample() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
