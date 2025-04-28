6e63330af88ff5a5dd98e9a94ca9d7eb88cede3c
User <user@example.com>
2025-04-28 13:42:12 +0300

    first commit

---
+++ Model/Event.swift
@@ -0,0 +1,8 @@
+//
+//  Event.swift
+//  Activity
+//
+//  Created by Bahar Atbaş on 28.04.2025.
+//
+
+import Foundation
---
+++ ViewModel/File.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  Activity
+//
+//  Created by Bahar Atbaş on 26.04.2025.
+//
+
+import Foundation
